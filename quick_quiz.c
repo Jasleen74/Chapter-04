a27ba3f0094ff233003c42fbac370eb8d71db598
Jasleen74 <preetjee@gmail.com>
2025-10-01 12:30:31 +0530

    (print natural numbers from 11 to 20 when the initial commit (value) of i is 0 )

---
+++ quick_quiz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+    /*Quick Quiz: Write a program to print natural numbers from 10 to 20 when initial loop
+ counter is initialized to 0. */
+
+     int i = 0;
+     while(i<=20){
+        if (i>10){     //here when the value of i will reach 10 the code will start executing then
+            printf("The value of i is %d\n", i);
+        }            // and after the value reaches 20 the loop will get EXECUTED
+
+        i ++;
+     }
+
+ 
+ 
+
+    
+  
+ return 0;
+}
